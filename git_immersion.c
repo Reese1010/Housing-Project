02b0f8240272502d21fb552a3d345574c641b5cc
Sreyas <sreyas.kodukulla@gmail.com>
2025-02-28 22:56:01 -0500

    final push

--- git_immersion.c
+++ git_immersion.c
@@ -3,6 +3,6 @@
 int main() {
     printf("Hello, world!\n");
     printf("This is to demonstrate pushing files to github!\n");
-    printf("This is another updated version.);
+    printf("This is another updated version.\n");
     return 0;
 }
