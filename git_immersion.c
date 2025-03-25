c6fa5ee7d74f9de46be199b45d80da60f69044af
Reese Allen <reese.allenx@gmail.com>
2025-03-25 17:13:38 -0400

    Delete git_immersion.c

--- git_immersion.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    printf("Hello, world!\n");
-    printf("This is to demonstrate pushing files to github!\n");
-    printf("This is another updated version.\n");
-    return 0;
-}
